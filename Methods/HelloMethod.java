47fd8f15272470aeabe5db523202da3cfcee19d1
HVsingh03 <harshvardhan200003@gmail.com>
2023-08-04 13:10:50 +0530

    sum method.

---
+++ Methods/HelloMethod.java
@@ -0,0 +1,12 @@
+package Methods;
+
+public class HelloMethod {
+    public static void main(String[] args) {
+        sum(1,3);
+    }
+    public static void sum(int a, int b) {
+        System.out.println(a+b);
+    }
+}
+
+
